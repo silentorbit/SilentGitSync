e2acf59406b66a58eeb7c890ae092bf00695d53e
Peter Hultqvist <hultqvist@silentorbit.com>
2020-09-07 07:09:36 +0200

    .

---
+++ Tools/Confirm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentOrbit.Tools
+{
+    public static class Confirm
+    {
+        public static bool Action(string message)
+        {
+        ask:
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            while (true)
+            {
+                var key = ReadKey("[Y]es, [R]etry or [A]bort");
+                switch (key)
+                {
+                    case ConsoleKey.Y:
+                        return true;
+                    case ConsoleKey.R:
+                        return false;
+                    case ConsoleKey.A:
+                        throw new Exception("User aborted");
+                    default:
+                        goto ask;
+                }
+            }
+        }
+
+        public static bool Retry(string message)
+        {
+        ask:
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            while (true)
+            {
+                var key = ReadKey("[R]etry, [I]gnore or [A]bort");
+                switch (key)
+                {
+                    case ConsoleKey.R:
+                        return true;
+                    case ConsoleKey.I:
+                        return false;
+                    case ConsoleKey.A:
+                        throw new Exception("User aborted");
+                    default:
+                        goto ask;
+                }
+            }
+        }
+
+        public static ConsoleKey ReadKey(string options)
+        {
+            //Empty previous key presses before prompting
+            while (Console.KeyAvailable)
+                Console.ReadKey();
+
+            Console.ResetColor();
+            Console.Write("    " + options + " ...");
+            var key = Console.ReadKey().Key;
+            Console.WriteLine();
+            return key;
+        }
+
+    }
+}
---
+++ Tools/RetryExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SilentOrbit.Tools
+{
+    public static class RetryExtensions
+    {
+        #region Retry helpers
+
+        public static void Retry(this Action action)
+        {
+            while (true)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Retrying... " + ex.Message);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        public static T Retry<T>(this Func<T> func)
+        {
+            while (true)
+            {
+                try
+                {
+                    return func();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
